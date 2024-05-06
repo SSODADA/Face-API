044f16798bd43937cb00152ba64ddaba899cfd07
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-05-07 00:14:35 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,11 +3,11 @@ const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 
 Promise.all([
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models') 
+    faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('models/'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
+    faceapi.nets.faceExpressionNet.loadFromUri('models/') 
 ]).then(startVideo);
 
 
