d7549d2d1284c939a93872ee5bdb0772ad07a95c
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-05-06 22:15:01 +0900

    Update script1.js

--- script1.js
+++ script1.js
@@ -7,12 +7,9 @@ Promise.all([
 ]).then(start)
 
 async function start() {
-    // Canvas container 생성
     const container = document.createElement('div')
     container.style.position = 'relative'
     document.body.append(container)
-
-    // 얼굴과 라벨을 매칭한다.
     const labeledFaceDescriptors = await loadLabeledImage()
     const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
     let image
@@ -22,34 +19,19 @@ async function start() {
     imageUpload.addEventListener('change', async () => {
         if (image) image.remove()
         if (canvas) canvas.remove()
-
-        // 사진을 화면에 표시함
         image = await faceapi.bufferToImage(imageUpload.files[0])
-
-        // 이미지 크기 제한
         if (image.width > 700 || image.height > 800) {
-           
-            // 이미지 크기를 조정
             image = await resizeImage(image, 700, 800)
         }
-
         container.append(image)
-
-        // canvas를 초기화 한다
         canvas = faceapi.createCanvasFromMedia(image)
         container.append(canvas)
         const displaySize = {width: image.width, height: image.height}
         faceapi.matchDimensions(canvas, displaySize)
-
-        // 사진에서 얼굴을 식별한다.
         const detections = await faceapi.detectAllFaces(image)
                                 .withFaceLandmarks()
                                 .withFaceDescriptors()
-        
-        // 사진에서 얼굴 좌표에 box를 그린다.
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
-
-        // 얼굴에 라벨을 표시한다.
         const result = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
         result.forEach((result, i) => {
             const box = resizedDetections[i].detection.box
@@ -58,21 +40,13 @@ async function start() {
         })
     })
 }
-
-// 이미지 크기 조정 함수
 async function resizeImage(image, maxWidth, maxHeight) {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-
-    // 가로 세로 비율을 유지하면서 최대 크기에 맞게 이미지 크기 조정
     const ratio = Math.min(maxWidth / image.width, maxHeight / image.height);
     canvas.width = image.width * ratio;
     canvas.height = image.height * ratio;
-
-    // 이미지 그리기
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-    // 이미지를 data URL로 반환
     return new Promise(resolve => {
         canvas.toBlob(blob => {
             const newImage = new Image();
@@ -83,16 +57,16 @@ async function resizeImage(image, maxWidth, maxHeight) {
 }
 
 function loadLabeledImage() {
-  const labels = ['백경민', 'Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
-  return Promise.all(
-      labels.map(async label => {
-          const description = []
-          const img = await faceapi.fetchImage('known/' + label + '.jpg')
-          const detections = await faceapi.detectSingleFace(img)
-                              .withFaceLandmarks()
-                              .withFaceDescriptor()
-          description.push(detections.descriptor)
-          return new faceapi.LabeledFaceDescriptors(label, description)
-      })
-  )
+    const labels = ['백경민', 'Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+    return Promise.all(
+        labels.map(async label => {
+            const description = []
+            const img = await faceapi.fetchImage('known/' + label + '.jpg')
+            const detections = await faceapi.detectSingleFace(img)
+                                .withFaceLandmarks()
+                                .withFaceDescriptor()
+            description.push(detections.descriptor)
+            return new faceapi.LabeledFaceDescriptors(label, description)
+        })
+    )
 }
