231a21b20b0ddd4df4d9f0b4c6af0343083ac140
Baek Kyung Min <80105027+SSODADA@users.noreply.github.com>
2024-05-06 22:53:56 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -5,7 +5,9 @@ document.body.appendChild(canvas);
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models') 
 ]).then(startVideo);
 
 
@@ -29,7 +31,9 @@ video.addEventListener('play', async () => {
 
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
         const labeledFaceDescriptors = await loadLabeledImage();
         const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
 
